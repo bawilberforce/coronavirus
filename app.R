bf6e6a0dc19d235a4340f531dbaf710390e5be87
Amara Wilberforce <b.a.wilberforce@outlook.com>
2021-07-13 14:05:24 +0100

    Functions separated out into separate file. Rolling average calculated using values +- 3 days from given date instead of 6 days previous. Tab layout put in. One graph displayed based on secondary area selection input.

--- app.R
+++ app.R
@@ -1,102 +1,38 @@
 
-# Load Packages -----------------------------------------------------------
+# Load Packages & Functions -----------------------------------------------------------
 
-library(dplyr)
-
-# FUNCTION: get_cases_data ----------------------------------------------------------
-
-
-get_cases_data<-function(area){
-    endpoint_regions <- paste0('https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=',area,'&structure={"Date":"date","Area":"areaName","NewCases":"newCasesBySpecimenDate"}')
-    httr::GET(
-        url = endpoint_regions,
-        httr::timeout(10)
-    ) -> response
-    return(response)
-    
-    if (response$status_code >= 400) {
-        err_msg <- httr::http_status(response)
-        stop(err_msg)
-        return(err_msg)
-    }
-}
-
-
-# FUNCTION: convert_to_json -----------------------------------------------
-
-
-convert_to_json<-function(response){
-    json_text <- httr::content(response, "text")
-    data      <- jsonlite::fromJSON(json_text)
-    dataset<-as.data.frame(data[["data"]])
-    dataset$Area<-as.factor(dataset$Area)
-    dataset$Date<-as.Date(dataset$Date)
-    return(dataset)}
-
-
-# FUNCTION: calculate_rolling_avg -----------------------------------------
-
-
-calculate_rolling_avg<-function(dataset){
-    area_breakdown<-purrr::map(levels(dataset$Area), ~filter(dataset, dataset$Area==.x))
-    names(area_breakdown)<-levels(dataset$Area)
-    area_breakdown_ordered<-lapply(area_breakdown, 
-                                   function(df){dplyr::arrange(df,Date)})
-    moving_averages<- lapply(area_breakdown_ordered, 
-                             function(df){(rolling_average=accelerometry::movingaves(df$NewCases,window=7))})
-    average_data<-lapply(area_breakdown_ordered, 
-                         function(df){cbind(df[7:length(df$Date),],moving_averages[unique(as.character(df$Area))])})
-    avg_dataset<-lapply(average_data,
-                         function(df){dplyr::rename(df,moving_avg=eval(names(df)[length(df)]))})
-combined_dataset<-data.frame(matrix(ncol=4)) 
-colnames(combined_dataset)<-c("Date", "Area", "NewCases", "moving_avg")
-    for (i in 1:length(avg_dataset)) {
-        combined_dataset<-rbind(avg_dataset[[i]],combined_dataset) %>%
-            na.omit()
-    }
-return(combined_dataset)
-}
-
-
-# FUNCTION: create_graph --------------------------------------------------
-
-create_graph<-function(dataset, start_date, end_date){
-    
-    filtered_data<-dataset%>%
-        dplyr::filter(Date>=as.Date(start_date) & Date<=as.Date(end_date))
-    filtered_data%>%ggplot2::ggplot(ggplot2::aes(x = Date, y = moving_avg)) + 
-        ggplot2::geom_point() +
-        ggplot2::geom_line() +
-        ggplot2::facet_grid(rows=filtered_data$Area) +
-        ggplot2::ylab("Rolling 7 Day Average of New Cases") +
-        ggplot2::xlab("Specimen Date")
-        }
-
+source("functions.R")
 
 
 
 # Create Shiny App --------------------------------------------------------
 
-ui <- shiny::fluidPage(
-    shiny::titlePanel("New Coronavirus Cases in the UK"),
-    shiny::sidebarPanel(
-        shiny::dateRangeInput("date_range", "Please select a date range:",
+ui<-fluidPage(mainPanel(
+    titlePanel("UK Coronavirus Data"),
+    sidebarPanel(
+    dateRangeInput("date_range", "Please select a date range:",
                               "2020-03-01", Sys.Date(),
                               max=Sys.Date(),min="2020-02-01"),
-        shiny::selectInput("area", "Plot data by:",
-                           choices = c("nation","region")), width=3
-    ),
-    shiny::mainPanel(
-        shiny::plotOutput("Plot1", height=800)
-        
-    )
-)
+    selectInput("area", "Plot data by:",
+                           choices = c("nation","region")), 
+    uiOutput("focus_area"),
+    width=4),
+    mainPanel(tabsetPanel(
+        tabPanel("New Cases", plotOutput("Plot1")),
+                 tabPanel("Hospitalisations",),
+                 tabPanel("Deaths",)))))
 
 server <- function(input, output){
-    output$Plot1<-shiny::renderPlot(get_cases_data(area=input$area)
+    output$focus_area <- renderUI({selectInput("focus_area", 
+                                                    "Show data for:", 
+                                                   get_cases_data(area=input$area)
+                                                   %>% convert_to_json()
+                                                   %>% select(Area)
+                                                   %>% unique())})
+    output$Plot1<- renderPlot(get_cases_data(area=input$area)
                                     %>% convert_to_json()
                                     %>% calculate_rolling_avg()
-                                    %>% create_graph(.,start_date=min(input$date_range),
+                                    %>% create_graph(.,focus_area=paste0(input$focus_area), start_date=min(input$date_range),
                                                      end_date=max(input$date_range)))
 }
 
---
+++ functions.R
@@ -0,0 +1,86 @@
+
+# Load Packages -----------------------------------------------------------
+
+library(tidyverse)
+library(ggplot2)
+library(shiny)
+
+# FUNCTION: get_cases_data ----------------------------------------------------------
+
+
+get_cases_data<-function(area){
+  endpoint_regions <- paste0('https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=',area,'&structure={"Date":"date","Area":"areaName","NewCases":"newCasesBySpecimenDate"}')
+  httr::GET(
+    url = endpoint_regions,
+    httr::timeout(10)
+  ) -> response
+  return(response)
+  
+  if (response$status_code >= 400) {
+    err_msg <- httr::http_status(response)
+    stop(err_msg)
+    return(err_msg)
+  }
+}
+
+
+# FUNCTION: convert_to_json -----------------------------------------------
+
+
+convert_to_json<-function(response){
+  json_text <- httr::content(response, "text")
+  data      <- jsonlite::fromJSON(json_text)
+  dataset<-as.data.frame(data[["data"]])
+  dataset$Area<-as.factor(dataset$Area)
+  dataset$Date<-as.Date(dataset$Date)
+  return(dataset)}
+
+
+# FUNCTION: calculate_rolling_avg -----------------------------------------
+
+
+calculate_rolling_avg<-function(dataset){
+  area_breakdown<-purrr::map(levels(dataset$Area), ~filter(dataset, dataset$Area==.x))
+  names(area_breakdown)<-levels(dataset$Area)
+  area_breakdown_ordered<-lapply(area_breakdown, 
+                                 function(df){arrange(df,Date)})
+  moving_averages<- lapply(area_breakdown_ordered, 
+                           function(df){(rolling_average=stats::filter(df, rep(1/7,7), sides=2))})
+  moving_averages_2<-lapply(moving_averages,
+                            function(df){(df[,3])})
+
+  average_data<-lapply(area_breakdown_ordered, 
+                       function(df){cbind(df,moving_averages_2[unique(as.character(df$Area))])})
+  avg_dataset<-lapply(average_data,
+                      function(df){rename(df,moving_avg=eval(names(df)[length(df)]))})
+  combined_dataset<-data.frame(matrix(ncol=4)) 
+  colnames(combined_dataset)<-c("Date", "Area", "NewCases", "moving_avg")
+  for (i in 1:length(avg_dataset)) {
+    combined_dataset<-rbind(na.omit(avg_dataset[[i]]),combined_dataset)
+  }
+  return(combined_dataset)
+}
+
+
+# FUNCTION: create_graph --------------------------------------------------
+
+create_graph<-function(dataset, focus_area, start_date, end_date){
+  
+  filtered_data_1<-dataset%>%
+    filter(Date>=as.Date(start_date) & Date<=as.Date(end_date))%>%
+    filter(Area==focus_area)%>%
+    select(Date, Cases=NewCases)
+  filtered_data_2<-dataset%>%
+    filter(Date>=as.Date(start_date) & Date<=as.Date(end_date))%>%
+    filter(Area==focus_area)%>%
+    select(Date, Cases=moving_avg)
+  
+  ggplot() + 
+    geom_col(data=filtered_data_1, mapping=aes(x=Date,
+                                               y=Cases))+
+    geom_line(data=filtered_data_2, mapping=aes(x=Date,
+                                                y=Cases),
+              colour="red", size=2)+
+    ylab("New Cases") +
+    xlab("Specimen Date")
+}
